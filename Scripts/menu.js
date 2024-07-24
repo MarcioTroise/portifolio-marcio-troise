dc84f159c84929bb57bfc07b13b94ac878eef514
Marcio Troise <mtroise15@gmail.com>
2024-07-24 20:52:10 -0300

    Continue adjust menu to desk mobile...

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,3 +1,24 @@
 // Condição de quando clicar no botão do menu
 
 // margin do body vai para 2% em tudo e fechar o menu com width 5px
+
+var menu = document.getElementById("btnOpenClose");
+
+function openCloseMenu(open, close) {
+    let openMenu, closeMenu
+    openMenu = open
+    closeMenu = close
+
+    if (open) {
+        console.log("Abriu")
+        console.log("menuu",menu)
+    }
+    else if (close) {
+        console.log("Fechou!")
+        return close
+    }
+    console.log(open, "ssssssss")
+
+}
+
+openCloseMenu()
