38fce1b55a5a69bf349661965d407baa2c1c50dc
Marcio Troise <mtroise15@gmail.com>
2024-08-02 21:07:17 -0300

    Changes on layout and menu... Contruct themes

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,15 +1,11 @@
 // Condição de quando clicar no botão do menu
 
-// margin do body vai para 2% em tudo e fechar o menu com width 5px
+// margin do body vai para 2% em tudo e fechar o menu com width 1px e padding de 5px
 
 var menuSide = document.getElementById("btnOpenClose");
 var menuClass = document.querySelector(".container-menu");
 
 menuClass.classList.remove("hiden")
 
-menuSide.addEventListener("click", (() => {
-    menuClass.classList.add("hiden")
-
-    if(!menuClass) console.log("fechando...")
-
+menuSide.addEventListener("", (() => {
 }));
