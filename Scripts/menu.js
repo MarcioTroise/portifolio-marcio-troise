739883c825710f8345723fbe9d95dc3bec169d02
Marcio Troise <mtroise15@gmail.com>
2024-08-07 21:22:14 -0300

    Change colors...

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -7,5 +7,23 @@ var menuClass = document.querySelector(".container-menu");
 
 menuClass.classList.remove("hiden")
 
-menuSide.addEventListener("", (() => {
-}));
+document.querySelector('.menu-abrir').onclick = function() {
+    document.documentElement.classList.add('menu-ativo');
+};
+
+document.querySelector('.menu-fechar').onclick = function() {
+    document.documentElement.classList.remove('menu-ativo');
+};
+
+var links = document.querySelectorAll(".menu-principal a");
+for (i = 0; i < links.length; i++) {
+  links[i].onclick = function() {
+    document.documentElement.classList.remove('menu-ativo');
+  }
+}
+
+document.documentElement.onclick = function(event) {
+    if (event.target === document.documentElement) {
+        document.documentElement.classList.remove('menu-ativo');
+    }
+};
