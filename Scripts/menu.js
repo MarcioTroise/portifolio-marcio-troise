194cc498534bbd7d1f8119aff6d4b3e1eac118be
Marcio Troise <mtroise15@gmail.com>
2024-08-13 23:50:32 -0300

    Test scripts menu...

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -3,27 +3,37 @@
 // margin do body vai para 2% em tudo e fechar o menu com width 1px e padding de 5px
 
 var menuSide = document.getElementById("btnOpenClose");
-var menuClass = document.querySelector(".container-menu");
+var menu = document.querySelector(".contact--hd");
+var page = document.querySelector(".page")
 
-menuClass.classList.remove("hiden")
+menu.classList.remove("hiden") // Menu Hiden
 
-document.querySelector('.menu-abrir').onclick = function() {
-    document.documentElement.classList.add('menu-ativo');
-};
 
-document.querySelector('.menu-fechar').onclick = function() {
-    document.documentElement.classList.remove('menu-ativo');
-};
+const open = menuSide.addEventListener('click', () => {
 
-var links = document.querySelectorAll(".menu-principal a");
-for (i = 0; i < links.length; i++) {
-  links[i].onclick = function() {
-    document.documentElement.classList.remove('menu-ativo');
-  }
-}
-
-document.documentElement.onclick = function(event) {
-    if (event.target === document.documentElement) {
-        document.documentElement.classList.remove('menu-ativo');
+  for(i=open; i != close; i) {
+    i[open] = () => {
+      menu.classList.add("hiden")
+      page.style.margin="3% 2% 0" 
+      close = false
     }
-};
+    i[close] = () => {
+      menu.classList.add("hiden")
+    page.style.margin="50px 2% 0 calc(250px + 2%)"
+    open = false
+    }
+  }
+
+  // if (open) {
+  //   menu.classList.add("hiden")
+  //   page.style.margin="3% 2% 0" 
+  //   close = false
+  // }
+  // else if (close) {
+  //   menu.classList.add("hiden")
+  //   page.style.margin="50px 2% 0 calc(250px + 2%)"
+  //   open = false
+  // }
+
+})
+
