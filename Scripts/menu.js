780550a27916caed219050e71b4157d01966272f
Marcio Troise <mtroise15@gmail.com>
2024-08-14 20:40:19 -0300

    Reform body and adjust Menu side to responsive mobile

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,39 +1,22 @@
 // Condição de quando clicar no botão do menu
+const toggleButton = document.getElementById("btnOpenClose");
+const sideMenu = document.getElementById('sideMenu');
+const page = document.getElementById("portfolio")
+const menu = document.querySelector(".contact--hd");
 
-// margin do body vai para 2% em tudo e fechar o menu com width 1px e padding de 5px
+// menu.classList.remove("hiden") // Menu Hiden
 
-var menuSide = document.getElementById("btnOpenClose");
-var menu = document.querySelector(".contact--hd");
-var page = document.querySelector(".page")
-
-menu.classList.remove("hiden") // Menu Hiden
-
-
-const open = menuSide.addEventListener('click', () => {
-
-  for(i=open; i != close; i) {
-    i[open] = () => {
-      menu.classList.add("hiden")
-      page.style.margin="3% 2% 0" 
-      close = false
+// Função para alternar a visibilidade do menu
+function toggleMenu() {
+    if (sideMenu.style.display == 'block' || sideMenu.style.display === '' || page.style.justifyContent == "" ) 
+      { // Esconde o menu se ele estiver visível       
+        page.style.justifyContent = "center";
+        sideMenu.style.display = 'none';
+    } else { // Mostra o menu se ele estiver escondido
+        page.style.justifyContent = "space-between";
+        sideMenu.style.display = 'block'; 
     }
-    i[close] = () => {
-      menu.classList.add("hiden")
-    page.style.margin="50px 2% 0 calc(250px + 2%)"
-    open = false
-    }
-  }
+}
 
-  // if (open) {
-  //   menu.classList.add("hiden")
-  //   page.style.margin="3% 2% 0" 
-  //   close = false
-  // }
-  // else if (close) {
-  //   menu.classList.add("hiden")
-  //   page.style.margin="50px 2% 0 calc(250px + 2%)"
-  //   open = false
-  // }
-
-})
-
+// Adiciona o evento de clique ao botão para alternar o menu
+toggleButton.addEventListener('click', toggleMenu);
