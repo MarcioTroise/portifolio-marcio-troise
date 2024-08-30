538f921326c6706e6f0a91adbf0886370276359d
Marcio Troise <mtroise15@gmail.com>
2024-08-30 20:19:31 -0300

    Create JS animation and start project container

--- Scripts/geometricsbg.js
+++ Scripts/geometricsbg.js
@@ -6,34 +6,38 @@ document.addEventListener("DOMContentLoaded", function() {
       const shape = document.createElement('div');
       
       // Definir tamanho aleatório para as formas
-      const size = Math.random() * 60 + 20; // Tamanho entre 50px e 150px
+      const size = Math.random() * 80 + 20; // Tamanho entre 50px e 150px
       shape.style.width = `${size}px`;
-      shape.style.height = `4px`;
-
+      shape.style.height = `${size}px`;
+      shape.style.position = 'absolute';
+      
       shape.classList.add('circle');
 
-      // Definir posição aleatória dentro do container
-      const posX = Math.random() * (container.clientWidth - size); // Subtrair o tamanho para evitar overflow
-      const posY = Math.random() * (container.clientHeight - size);
+      // Definir posição aleatória dentro do container (inicial embaixo)
+      const posX = Math.random() * (container.clientWidth - size);
       shape.style.left = `${posX}px`;
-      shape.style.top = `${posY}px`;
-      
+      shape.style.top = `${container.clientHeight}px`; // Começa de baixo
+
       // Cor de fundo aleatória
-      const colors = [ '#250937f2', '#0188bd'];
-      const randomColor = colors[Math.floor(Math.random() * colors.length)];  
+      const colors = [ '#250937f2', '#0188bd', '#6dfff3'];
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
       shape.style.backgroundColor = randomColor;
 
-      shape.classList.add('shape'); // Adicionar a classe 'shape' ao final
+      // Adicionar classes de animação e forma
+      shape.classList.add('shape');
       container.appendChild(shape);
+
+      // Aplicar a animação de movimento
+      shape.style.animation = `moveUp ${Math.random() * 15 + 5}s linear infinite`;
    }
+
    // Iterar por todos os containers e adicionar formas a cada um
    containers.forEach(function(container) {
       // Gerar quantidades de formas geométricas aleatórias para cada container
       // let variableSize =  (containers[0].clientHeight -  containers[0].clientWidth) / 10
       // console.log('TESTE S',(containers[0]))
-      for (let i = 0; i < 70; i++) {
+      for (let i = 0; i < 30; i++) {
          createShape(container);
       }
    });
-
 });
